1973ac997a54fdaf22c95d016c68889b2a188f4b
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-12-02 13:03:48 -0600

    add part2 of day 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,7 +1,11 @@
+#[macro_use]
+extern crate itertools;
+
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::iter::FromIterator;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,22 +13,54 @@ fn main() {
     let filename = &args[1];
 
     println!("{}", checksum(&filename));
+    println!("{}", one_off(&filename));
 }
 
-fn checksum(filename: &str) -> i64 {
+fn get_contents(filename: &str) -> String {
     let mut f = File::open(filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
-    let lines = contents.lines();
-    let mut num_two_times = 0;
-    let mut num_three_times = 0;
+    contents
+}
+
+fn one_off(filename: &str) -> String {
+    let contents = get_contents(filename);
+    let lines: Vec<&str> = contents.split('\n').collect();
+
+    let mut ret: Vec<char> = Vec::new();
+
+    for (word1, word2) in iproduct!(&lines, &lines) {
+        let mut num_diff = 0;
+        for (ch1, ch2) in izip!(word1.chars(), word2.chars()) {
+            if ch1 != ch2 {
+                num_diff += 1;
+            }
+        }
+        if num_diff == 1 {
+            for (ch1, ch2) in izip!(word1.chars(), word2.chars()) {
+                if ch1 == ch2 {
+                    ret.push(ch1);
+                }
+            }
+            break;
+        }
+    }
+
+    String::from_iter(ret.to_owned())
+}
+
+fn checksum(filename: &str) -> i64 {
+    let contents = get_contents(filename);
+
+    let mut num_two_times: i64 = 0;
+    let mut num_three_times: i64 = 0;
     let mut has_two_times: bool;
     let mut has_three_times: bool;
 
-    for line in lines {
+    for line in contents.lines() {
         let mut unique_chars: HashMap<char, i64> = HashMap::new();
         for character in line.chars() {
             *unique_chars.entry(character).or_insert(0) += 1
@@ -56,7 +92,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example() {
+    fn part1_example() {
         assert!(checksum("test") == 12);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert!(one_off("test2") == "fgij");
     }
 }
