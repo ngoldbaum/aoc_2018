ea22aff1c9cfe9f9ac5aaf49f4f4544042018316
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-12-02 00:17:39 -0600

    initial commit

---
+++ day_1/src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
