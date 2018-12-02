c51c7cc05dabf8776b24510cb7367bd7c54719c6
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-12-02 00:48:48 -0600

    running first half of day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -7,11 +7,36 @@ fn main() {
 
     let filename = &args[1];
 
+    println!("{}", get_frequency(&filename))
+}
+
+fn get_frequency(filename: &str) -> i64 {
     let mut f = File::open(filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
+    
 
-    println!("With text:\n{}", contents);
+    let lines = contents.lines();
+
+    let mut frequency: i64 = 0;
+
+    for line in lines {
+        frequency += line.parse::<i64>().unwrap();
+    }
+
+    frequency
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn examples() {
+        assert!(get_frequency("test1") == 3);
+        assert!(get_frequency("test2") == 0);
+        assert!(get_frequency("test3") == -6);
+    }
 }
