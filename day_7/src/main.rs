0f11b57f17f2ad4f19d7890336630899f234cd63
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-12-07 10:38:32 -0600

    adding I/O

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+
+    let contents = get_contents(filename);
+    
+    println!("{}", contents);
+}
+
+fn get_contents(filename: &str) -> String {
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    contents
 }
