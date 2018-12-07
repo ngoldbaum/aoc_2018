8e65d1b89b990a904e399cd734a5c373a877162d
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-12-07 10:45:58 -0600

    proper error handling

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -3,22 +3,23 @@ use std::fs::File;
 use std::io::prelude::*;
 
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
 
     let filename = &args[1];
 
-    let contents = get_contents(filename);
-    
+    let contents = get_contents(filename)?;
+
     println!("{}", contents);
+    
+    Ok(())
 }
 
-fn get_contents(filename: &str) -> String {
-    let mut f = File::open(filename).expect("file not found");
+fn get_contents(filename: &str) -> Result<String, std::io::Error> {
+    let mut f = File::open(filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
-    contents
+    Ok(contents)
 }
